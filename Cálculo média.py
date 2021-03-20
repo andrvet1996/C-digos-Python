f00b22a544c1f3256aa39771f7f2de2b54a4d859
A.L.F. SANTOS <80923022+andrvet1996@users.noreply.github.com>
2021-03-19 22:29:58 -0300

    Add files via upload

---
+++ Cálculo média.py
@@ -0,0 +1,10 @@
+N1=input('Digite a sua nota 1:')
+N2=input('Digite sua nota 2:')
+nt1=eval(N1)
+nt2=eval(N2)
+media=(0.4*nt1)+(0.6*nt2)
+print (media)
+if media>=5.0:
+    print('Você foi aprovado.Parabéns!!!')
+else:
+    print('Você foi reprovado. É preciso estudar mais')
