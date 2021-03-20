d7857a0ab4c093c182f8b81efde29365789ce6f5
A.L.F. SANTOS <80923022+andrvet1996@users.noreply.github.com>
2021-03-20 10:12:59 -0300

    Update Cálculo média.py

--- Cálculo média.py
+++ Cálculo média.py
@@ -2,9 +2,11 @@ N1=input('Digite a sua nota 1:')
 N2=input('Digite sua nota 2:')
 nt1=eval(N1)
 nt2=eval(N2)
-media=(0.4*nt1)+(0.6*nt2)
-print (media)
+
+print (f'A média é: {media}')media=(0.4*nt1)+(0.6*nt2)
+
 if media>=5.0:
     print('Você foi aprovado.Parabéns!!!')
+    
 else:
     print('Você foi reprovado. É preciso estudar mais')
