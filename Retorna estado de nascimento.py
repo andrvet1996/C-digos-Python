a92aa338f1b35476e993b0b2a53d1e0101a6884e
A.L.F. SANTOS <80923022+andrvet1996@users.noreply.github.com>
2021-03-27 11:25:41 -0300

    Create Retorna estado de nascimento.py

---
+++ Retorna estado de nascimento.py
@@ -0,0 +1,9 @@
+def estadoNasc(presidente):  # 'retorna o estado de nascimento do presidente informado'
+    estados = {'Barack Hussein Obama II': 'Hawaii', 'George Walker Bush': 'Connecticut',
+               'William Jefferson Clinton': 'Arkansas',
+               'George Herbert Walker Bush': 'Massachussetts',
+               'Ronald Wilson Reagan': 'Illinois',
+               'James Earl Carter, Jr': 'Georgia'}
+    return estados [presidente]
+
+#estadoNasc('James Earl Carter') interpretador
