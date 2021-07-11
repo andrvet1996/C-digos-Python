163a8254d4b1c1df4d4fef143f0ba5bf7cf482ee
A.L.F. SANTOS <80923022+andrvet1996@users.noreply.github.com>
2021-07-10 21:39:15 -0300

    Create Curso Guanabara-primeiros comandos.py

---
+++ Curso Guanabara-primeiros comandos.py
@@ -0,0 +1,18 @@
+#PRIMEIROS COMANDOS
+
+#DESAFIO 1
+#nome = input ('Qual é o seu nome?')
+#print ("Olá",nome,'!','Prazer em te conhecer!')
+
+#DESAFIO 2
+#dia = input ('Em que dia você nasceu?')
+#mês = input ('Em que mês você nasceu (escreva o mês)?')
+#ano = input ('Em que ano você nasceu?')
+#print ('Você nasceu no dia', dia, "do", mês, 'de', ano,'. Correto?')
+
+
+#DESAFIO 3
+#n1 = eval (input ('Digite o primeiro número:'))
+#n2 = eval (input ('Digite o segundo número:'))
+#soma = n1 + n2
+#print ('A soma é', soma,'.')
