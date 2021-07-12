4259f2fc940eab87caa26baa934091e322d9f105
A.L.F. SANTOS <80923022+andrvet1996@users.noreply.github.com>
2021-07-12 20:49:29 -0300

    Update Curso Guanabara.py

--- Curso Guanabara.py
+++ Curso Guanabara.py
@@ -167,6 +167,21 @@ n10 = n * 10
 print ('A tabuada no número digitado e: {},{},{},{},{},{},{},{},{},{}.'
 .format (n1,n2,n3,n4,n5,n6,n7,n8,n9,n10))
 
+#outra forma, sem criar vriáveis
+n = int (input ('Digite um número para ver sua tabuada: '))
+print ('*' * 12)
+print ('{} * {:2} = {}'.format (n, 1, n*1))
+print ('{} * {:2} = {}'.format (n, 2, n*2))
+print ('{} * {:2} = {}'.format (n, 3, n*3))
+print ('{} * {:2} = {}'.format (n, 4, n*4))
+print ('{} * {:2} = {}'.format (n, 5, n*5))
+print ('{} * {:2} = {}'.format (n, 6, n*6))
+print ('{} * {:2} = {}'.format (n, 7, n*7))
+print ('{} * {:2} = {}'.format (n, 8, n*8))
+print ('{} * {:2} = {}'.format (n, 9, n*9))
+print ('{} * {:2} = {}'.format (n, 10, n*10))
+print ('*' * 12)
+
 
 #Desafio 10 conversão real/dólar
 r = eval (input('Quantos reais você possui? '))
