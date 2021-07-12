3a757d5d9f942c340ccfa9afbc69f53ca5ddea48
A.L.F. SANTOS <80923022+andrvet1996@users.noreply.github.com>
2021-07-12 16:16:28 -0300

    Update Curso Guanabara.py

--- Curso Guanabara.py
+++ Curso Guanabara.py
@@ -149,3 +149,17 @@ cm = m * 100
 mm = cm * 10
 print ('O valor em centrímetros e milímetros são, respectivamente, {}cm e {}mm.'. format (cm, mm))
 
+#Desafio 9 tabuada
+n = int (input ('Digite um número: '))
+n1 = n * 1
+n2 = n * 2
+n3 = n * 3
+n4 = n * 4
+n5 = n * 5
+n6 = n * 6
+n7 = n * 7
+n8 = n * 8
+n9 = n * 9
+n10 = n * 10
+print ('A tabuada no número digitado e: {},{},{},{},{},{},{},{},{},{}.'
+.format (n1,n2,n3,n4,n5,n6,n7,n8,n9,n10))
