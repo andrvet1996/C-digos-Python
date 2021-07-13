e01a2829334bb8320fbc36f751b4717b0c76f97a
A.L.F. SANTOS <80923022+andrvet1996@users.noreply.github.com>
2021-07-13 16:49:39 -0300

    Update Curso Guanabara.py

--- Curso Guanabara.py
+++ Curso Guanabara.py
@@ -10,51 +10,39 @@
 #ano = input ('Em que ano você nasceu?')
 #print ('Você nasceu no dia', dia, "do", mês, 'de', ano,'. Correto?')
 
-
 #DESAFIO 3
 #n1 = eval (input ('Digite o primeiro número:'))
 #n2 = eval (input ('Digite o segundo número:'))
 #soma = n1 + n2
 #print ('A soma é', soma,'.')
 
-
 #EXERCÍCIO 2 (parece o desafio 1)
-
 #nome = input ('Digite seu nome: ')
 #print ('É um prazer te conhecer, {}!'.format(nome)) 
 
 
 #TIPOS PRIMITIVOS (int, float, bool=True or False, str)
-
 #Exemplo 1
-
 #n1 = int (input ('Digite o primeiro número:'))
 #n2 = int (input ('Digite o segundo número:'))
 #s = n1+n2
-
 #print ('A soma entre', n1, 'e', n2, 'vale', s) 
 
-
 #Desafio 2 is. alguma coisa
-
 #n = input ('Digite algo:')
 #print (n.isnumeric()) #confira se é inteiro
 #print (n.isalpha())#confirma se é letra
 #print (n.isalnum())#confirma se é letra e número 3a por exemplo
 #print (n.isupper())
 
-
 #Desafio 3 soma de números e apresentar resultado
-
 n1 = int (input ('Digite o primeiro número: '))
 n2 = int (input ('Digite o segundo número:  '))
 n3 = int (input ('Digite o terceiro número: '))
 s = n1+n2+n3
 print ('A soma de {}, {} e {} é {}'. format (n1, n2, n3, s))
 
-
 #EXERCÍCIO 4 dissecando uma variável
- 
 #a = input ('Digite algo: ')
 #print ('O tipo primitivo desse valor é', type(a))
 #print ('Só possui espaço?', a.isspace())
@@ -76,19 +64,14 @@ print(f'Todas as letras são maiúsculas? {a.isupper()}')
 print(f'Todas as letras são minúsculas? {a.islower()}')
 print(f'Está capitalizada? {a.istitle()}')
 
-
-
 #OPERADORES ARITMÉTICOS
-
 #a = 2**3
 #a = pow (2,3) isso é igual a 2**3
 #print (a)
 
-
 #raiz quadrada
 #a = 25**(1/2)
 #print (a)
-
 
 #raiz cúbica
 #a = 27**(1/3)
@@ -116,9 +99,7 @@ print(f'Está capitalizada? {a.istitle()}')
 #na divisão vai aparecer duas casas decimais
 #print ('A divisão inteira é {} e a potência é {}.'. format (di, e))
 
-
 #Desafio 5 sucessor e antecessor
-
 n = int (input ('Escolha um número inteiro: '))
 n1 = n - 1
 n2 = n + 1
@@ -130,16 +111,13 @@ n = int (input('Digite um número: '))
 print ('Analisando o valor {}, seu antecessor é {} e o sucessor é {}.'.format (n, (n-1), (n+1)))
 
 #Desafio 6 
-
 #n = int (input ('Digite um número: '))
 #dn = n * 2
 #tn = n * 3
 #rqn = n ** (1/2)
 #print ('O dobro do número, o triplo e a raiz quadrada são, respectivamente {}, {} e {}.'.format (dn, tn, rqn))
 
-
 #Desafio 7
-
 #n1 = int (input ('Nota 1: '))
 #n2 = int (input ('Nota 2: '))
 #M = (n1 + n2) / 2
@@ -182,14 +160,12 @@ print ('{} * {:2} = {}'.format (n, 9, n*9))
 print ('{} * {:2} = {}'.format (n, 10, n*10))
 print ('*' * 12)
 
-
 #Desafio 10 conversão real/dólar
 r = eval (input('Quantos reais você possui? R$ '))
 d = r / 3.27
 print ('A sua quantia reais corresponde a {:.2f} dólares.'. format (d))
 
 #Desafio 11 área para pintar
-
 l = eval (input ('Qual a largura em metros? '))
 h = eval (input ('Qual a altura em metros? ')) 
 a = l * h
@@ -206,3 +182,4 @@ print ('O preço com desconto de 5% é R${:.2f}.'.format (npcd))
 s = eval (input ('Digite seu salário atual: '))
 ns = s + (s * 0.15)
 print ('Seu salário com o aumento de 15% é R$ {:.2f}.'.format (ns))
+
