3756cd0d1ed534af897e595afee56131b0a445bc
A.L.F. SANTOS <80923022+andrvet1996@users.noreply.github.com>
2021-07-19 17:37:55 -0300

    Update Curso Guanabara-módulo 01.py

--- Curso Guanabara-módulo 01.py
+++ Curso Guanabara-módulo 01.py
@@ -525,3 +525,21 @@ else:
 distância = float (input ('Qual é a distância até o seu destino, em Km? '))
 p = distância * 0.5 if distância <=200 else distância * 0.45 
 print ('O preço da passagem será de R$ {:.2f}.'.format (p))
+
+
+#Desafio 33-maior e menor valor
+
+n1 = int (input ('Digite o primeiro número: '))
+n2 = int (input ('Digite o segundo número:  '))
+n3 = int (input ('Digite o terceiro número: '))
+#verificando quem é menor
+menor = n1
+if n2 < n1 and n2 < n3:
+    menor = n2
+#verificando que é o maior
+maior = n1
+if n2 > n1 and n2 > n3:
+    maior = n2
+if n3 > n1 and n3 > n2:
+    maior = n3
+print ('O menor valor digitado é {} e o maior é {}.'.format (menor, maior))
