92ad4c3ea63448f38ed02f889538f07b60f2fb6e
A.L.F. SANTOS <80923022+andrvet1996@users.noreply.github.com>
2021-07-21 10:36:19 -0300

    Update Curso Guanabara-módulo 02.py

--- Curso Guanabara-módulo 02.py
+++ Curso Guanabara-módulo 02.py
@@ -20,3 +20,19 @@ if valprest <= lim30:
     print ('O empréstimo para adquirir uma casa de {:.2f}, a ser pago em {} parcelas mensais, no valor de R$ {:.2f} foi APROVADO!'.format (v, t, valprest))
 else:
     print ('O empréstimo NÃO FOI APROVADO, pois o valor da prestação ultrapassa 30% de sua renda mensal.')
+    
+Dsafio 37- conversor de decimal para binário, octal e hexadecimal    
+num = int (input ('Digite um número inteiro: '))
+print('''Escolha uma das bases para conversão: 
+[1] converter para BINÁRIO 
+[2] converter para OCTAL 
+[3] converter para HEXADECIMAL''')
+opção = int(input('Sua opção: '))
+if opção == 1:
+    print('{} convertido para BINÁRIO é igual a {}.'.format (num, bin(num)[2:]))#[2;0] retira algarismos desnecessários
+elif opção == 2:
+    print('{} convertido para OCTAL é igual a {}.'.format(num, oct(num)[2:]))
+elif opção == 3:
+    print('{} convertido para HEXADECIMAL é igual a {}.'.format(num, hex(num)[2:]))
+else:
+    print('Opção inválida! Tente novamente.')
