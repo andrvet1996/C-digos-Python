5e0af0cd60cf78c0f09ec0ecb7fe792ab84101ad
A.L.F. SANTOS <80923022+andrvet1996@users.noreply.github.com>
2021-07-22 18:12:10 -0300

    Update Curso Guanabara-módulo 02.py

--- Curso Guanabara-módulo 02.py
+++ Curso Guanabara-módulo 02.py
@@ -141,3 +141,18 @@ if a < b + c and b < a + c and c < a + b:
         print('O triângulo é ISÓSCELES.')
 else:
     print('Os segmentos {}, {} e {} NÃO FORMAM UM TRIÂNGULO')
+          
+#Desafio 43-IMC
+peso = float (input ('Digite o seu peso (Kg): '))
+alt = float (input ('Digite sua altura (m): '))
+imc = peso / (alt**2)
+if imc < 18.5:
+    print ('Seu IMC é {:.1f}. Você está ABAIXO DO PESO IDEAL.'.format (imc))
+elif 18.5 <= imc < 25.0:
+    print ('Seu IMC é {:.1f}. O seu peso está NORMAL.'.format (imc))
+elif 25.0 <= imc <  30.0:
+    print ('Seu IMC é {:.1f}. Você está com SOBREPESO.'.format (imc))
+elif 30.0 <= imc < 40.0:
+    print ('Seu IMC é {:.1f}. Você está OBESO.'.format (imc))
+else:
+    print ('Seu IMC é {:.1f}. Você está com OBESIDADE MÓRBIDA.'.format (imc))
