4d91c341b66a60a128414915007293908a3101bb
A.L.F. SANTOS <80923022+andrvet1996@users.noreply.github.com>
2021-08-07 12:50:55 -0300

    Update Curso Guanabara-módulo 02.py

--- Curso Guanabara-módulo 02.py
+++ Curso Guanabara-módulo 02.py
@@ -848,4 +848,31 @@ elif computador == 2:
         print('O computador vence!')
     else:
         print ('Jogada inválida!')
+       
 
+#Desafio 71-caixa eletrônico
+print('=' * 30)
+print('{:^30}'.format('BANCO CEV'))  # comando para centralizar
+print('=' * 30)
+valor = int(input('Quanto você quer sacar? R$'))
+total = valor
+céd = 50
+totcéd = 0
+while True:
+    if total >= céd:
+        total -= céd
+        totcéd += 1
+    else:
+        if totcéd > 0:
+            print(f'Total de {totcéd} cédula(-s) de R${céd}.')
+        elif céd == 50:
+            céd = 20
+        elif céd == 20:
+            céd = 10
+        elif céd == 10:
+            céd = 1
+        totcéd = 0
+        if total == 0:
+            break
+print('=' * 30)
+print('Volte sempre ao BANCO CEV! Tenha um bom dia!')
