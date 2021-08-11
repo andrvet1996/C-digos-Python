8a3012d4df2c79c4f8c5fe9824510960c5118588
A.L.F. SANTOS <80923022+andrvet1996@users.noreply.github.com>
2021-08-11 12:24:53 -0300

    Update Curso Guanabara-módulo 03.py

--- Curso Guanabara-módulo 03.py
+++ Curso Guanabara-módulo 03.py
@@ -1,1 +1,37 @@
+#Aula 16-TUPLAS símbolo ()
+#AS TUPLAS SÃO IMUTAVEIS#
+
+'''lanche = ('hambúrger','suco','pizza','pudim') #pode ser sem parênteses
+print(lanche)
+print(lanche[1])
+print(lanche[-4])
+print(lanche[1:3])#retira o elemento 3
+print(lanche[2:])
+print(lanche[:2])#mostra o elemento 0 e 1 e retira o 2
+
+for comida in lanche:
+    print ('Eu vou comer {}.'.format(comida))#print(f'Eu vou comer {comida}'')
+print('Comi pra caramba!')
+
+for cont in range (0, len(lanche)):
+    print (lanche[cont])#se colocar só o cont imprimi os números
+print('Comi pra caramba!')
+
+#PARA MOSTRAR A POSIÇÃO
+
+for pos, comida in enumerate (lanche):
+    print ('Eu vou comer {} na posição {}.'.format(comida, pos))
+    
+lanche = ('hambúrger','suco','pizza','pudim')
+print (sorted(lanche)) # ordena a tupla'''
+    
+a = (2,5,4)
+b = (5,8,1,2)
+c = (a + b)
+print(c)
+print(len(c))#conta o numero de elementos
+print(c.count(5))#quantas x aparece 5 na tupla c
+print(c.index(8))#mostra a posiçao do elemento
+
+#del (c) esse comando apaga a tupla toda
 
