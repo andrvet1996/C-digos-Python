f11c1cd77c94ecc5ac1984bcb6b3762933297efc
A.L.F. SANTOS <80923022+andrvet1996@users.noreply.github.com>
2021-08-13 16:39:39 -0300

    Update Curso Guanabara-módulo 03.py

--- Curso Guanabara-módulo 03.py
+++ Curso Guanabara-módulo 03.py
@@ -35,3 +35,18 @@ print(c.index(8))#mostra a posiçao do elemento
 
 #del (c) esse comando apaga a tupla toda
 
+
+#Desafio 72-número por extenso
+cont = ('zero', 'um','dois','três','quatro',
+'cinco', 'seis','sete','oito','nove',
+'dez','onze','doze','treze','catorze',
+'quinze','dezesseis','dezessete','dezoito','dezenove','vinte')
+
+while True:
+    núm = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= núm <= 20:
+        break
+    print ('Tente novamente.', end= '')#end = '' não pular linha
+    
+print ('Você digitou o número {}.'.format (cont[núm]))
+#print (f'Você digitou o número {cont[núm]})
