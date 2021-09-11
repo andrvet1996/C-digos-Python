f09596415487aa66397cbe645770ed1efce53cd3
A.L.F. SANTOS <80923022+andrvet1996@users.noreply.github.com>
2021-09-11 16:45:40 -0300

    Update Curso Guanabara-módulo 03.py

--- Curso Guanabara-módulo 03.py
+++ Curso Guanabara-módulo 03.py
@@ -268,3 +268,24 @@ if 5 in lista:
     print('O valor 5 faz parte da lista.')
 else:
     print('O valor 5 não faz parte da lista.')
+    
+    
+#Desafio 82-dividindo valores em várias listas
+num = list()
+pares = list()
+ímpares = list()
+while True:
+    num.append(int(input('Digite um número: ')))
+    resp = str(input('Quer continuar? [S/N]: ')) .upper()
+    if resp in 'N':
+        break
+for i, v in enumerate(num):
+    if v % 2 == 0:
+        pares.append(v)
+    else:
+        ímpares.append(v)
+print('*=' * 30)
+print(f'A lista dos valores digitados foi {num}.')
+print(f'A lista dos valores pares foi {pares}.')
+print(f'A lista dos valores ímpares foi {ímpares}.')
+print('*=' * 30)
