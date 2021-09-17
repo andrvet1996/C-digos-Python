635843de2d46bcc029fe8e4b9730032cffeb76f2
A.L.F. SANTOS <80923022+andrvet1996@users.noreply.github.com>
2021-09-17 10:54:59 -0300

    Update Curso Guanabara-módulo 03.py

--- Curso Guanabara-módulo 03.py
+++ Curso Guanabara-módulo 03.py
@@ -289,3 +289,23 @@ print(f'A lista dos valores digitados foi {num}.')
 print(f'A lista dos valores pares foi {pares}.')
 print(f'A lista dos valores ímpares foi {ímpares}.')
 print('*=' * 30)
+
+
+#Desafio 83-validando expressões matemáticas
+
+expr = str(input('Digite a expressão: '))
+pilha = []
+for simb in expr:
+    if simb == '(':
+        pilha.append('(')
+    elif simb == ')':
+        if len (pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append (')')
+            break
+if len (pilha) == 0:
+    print ('Sua expressão é válida!')
+else:
+    print ('Sua expressão está errada!')
+    
