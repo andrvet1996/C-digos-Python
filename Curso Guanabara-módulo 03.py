376cda2c9f7bdd0675303ae747a6edfafc26d388
A.L.F. SANTOS <80923022+andrvet1996@users.noreply.github.com>
2021-09-22 11:05:09 -0300

    Update Curso Guanabara-módulo 03.py

--- Curso Guanabara-módulo 03.py
+++ Curso Guanabara-módulo 03.py
@@ -365,3 +365,36 @@ for p in galera:
         totmenor += 1
       
 print ('Temos {} pessoas maiores e {} menores de idade.'.format(totmaior, totmenor))
+
+#Desafio 84-lista composta e análise de dados
+temp = []
+princ = []
+maior = menor = 0
+
+while True:
+    temp.append(str(input('Nome: ')))#p[0]
+    temp.append(float(input('Peso: ')))#p[1]
+    if len(princ) == 0:
+        maior = menor = temp[1]
+    else:
+        if temp[1] > maior:
+            maior = temp[1]
+        if temp[1] < menor:
+            menor = temp[1]
+    princ.append(temp[:])
+    temp.clear()
+    resp = str(input('Quer continuar?[S/N]')).upper().strip()
+    if resp in 'N':
+        break
+print('=-' * 30)
+print('Ao todo você cadastrou {} pessoas.'.format(len(princ)))#len dispensa contador
+print(f'O maior peso foi de {maior} Kg. Peso de', end='')
+for p in princ:
+    if p[1] == maior:
+        print(f'[{p[0]}]', end='')
+print()
+print(F'O menor peso foi de {menor} Kg. Peso de', end='')
+for p in princ:
+    if p[1] == menor:
+        print(f'[{p[0]}]', end='')
+
