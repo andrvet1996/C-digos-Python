64661888b9a2dab9ad6afa98b6686ff4f30d4c17
A.L.F. SANTOS <80923022+andrvet1996@users.noreply.github.com>
2021-10-09 11:14:44 -0300

    Update Curso Guanabara-módulo 03.py

--- Curso Guanabara-módulo 03.py
+++ Curso Guanabara-módulo 03.py
@@ -431,3 +431,30 @@ for l in range(0, 3):
     print()
 
 
+#Desafio 87-calculos em matrizes
+
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+spar = mai = scol = 0
+for l in range(0, 3):
+    for c in range(0, 3):
+        matriz[l][c] = int(input('Digite um valor para {}, {}: '.format(l, c)))
+print('=*' * 30)
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f'[{matriz[l][c]:^5}]', end='')
+        if matriz[l][c] % 2 ==0:
+            spar += matriz[l][c]
+    print()
+print('=*' * 30)
+print(f'A soma dos valores pares é {spar}.')
+for l in range(0,3): #soma da terceira coluna e a linha é fica-segunda linha
+    scol += matriz [l][2]
+print(f'A soma dos elementos da terceira coluna é {scol}.')
+
+for c in range(0,3):
+    if c == 0:
+        mai = matriz[l][c]
+    elif matriz[l][c] > mai:
+        mai = matriz[l][c]
+
+print(f'O maior elemento da lista é o {mai}.')
