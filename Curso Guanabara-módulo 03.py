fd512f73377341a35840944c8a527b5d4475ebc6
A.L.F. SANTOS <80923022+andrvet1996@users.noreply.github.com>
2021-10-12 10:43:21 -0300

    Update Curso Guanabara-módulo 03.py

--- Curso Guanabara-módulo 03.py
+++ Curso Guanabara-módulo 03.py
@@ -459,6 +459,32 @@ for c in range(0,3):
 
 print(f'O maior elemento da lista é o {mai}.')
 
+# Desafio 88-palpites para a Mega Sena:
+from random import randint
+lista = []
+jogos = []
+print('-=' * 30)
+print('          JOGA NA MEGA SENA           ')
+print('-=' * 30)
+quant = int(input('Quantos jogos você quer que eu sorteie? '))
+tot = 1
+while tot <= quant:
+    cont = 0
+    while True:
+        num = randint(1, 60)
+        if num not in lista:
+            lista.append(num)
+            cont += 1
+            if cont >= 6:
+                break
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    tot += 1
+print('-=' * 3, f'SORTEANDO {quant} JOGOS', '-=' * 3)
+for i, l in enumerate(jogos):
+    print(f'Jogo {i+1}: {l}')
+
 #AULA 19-DICIONÁRIO
 
 pessoas = {'nome': 'Gustavo', 'sexo': 'M', 'idade': 22}
