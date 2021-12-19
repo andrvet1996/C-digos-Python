7e762dd74b200d49803c296218449914557aada8
A.L.F. SANTOS <80923022+andrvet1996@users.noreply.github.com>
2021-12-19 12:17:14 -0300

    Update Curso Guanabara-módulo 03.py

--- Curso Guanabara-módulo 03.py
+++ Curso Guanabara-módulo 03.py
@@ -762,3 +762,77 @@ while True:
 print('-'*40)
 print('Volte sempre')
         
+
+ #Aula 20-Funções
+
+'''def lin():#não executa
+    print('-+' * 30)
+
+#programa principal
+lin()
+print('SISTEMA DE ALUNOS')
+lin()
+print('CADASTRO DE FUNCIONÁRIOS')
+lin()
+print('ERRO DO SISTEMA')
+
+def título (txt):
+    print('-' * 30)
+    print(txt)
+    print('-' * 30)
+
+#programa principal para
+título('     CURSO EM VÍDEO     ')
+título('     APRENDA PYTHON     ')
+título('     GUSTAVO GUANABARA  ')'''
+
+'''a = 4
+b = 5
+s = a + b
+print(s)
+a = 8
+b = 9
+s = a + b
+print(s)
+a = 2
+b = 1
+s = a + b
+print(s)
+
+def soma (a, b):
+    print(f' A = {a} e B = {b}')
+    soma = (a + b)
+    print(f' A soma A + B = {soma}')
+
+#programa principal
+soma(4,5)
+soma(2,9)
+soma(2,1)
+
+def contador(* núm):
+    print(núm)
+    tam = len(núm)
+    print(f'Recebi os valores {núm} e são ao todos {tam} valores')
+contador(2, 1, 7)
+contador(8, 0)
+contador(4, 4, 7, 6, 2)
+
+def dobra(lst):
+    pos = 0 #variável
+    while pos < len(lst):
+        lst[pos] *= 2
+        pos += 1
+# programa principal
+valores = [7, 2, 5, 0, 4]
+dobra(valores)
+print(valores)'''
+
+
+def soma(* valores):
+    s = 0
+    for num in valores:
+        s += num
+    print(f'Somando os valores {valores} temos {s}.')
+
+soma(5, 2)
+soma(2, 9, 4)
