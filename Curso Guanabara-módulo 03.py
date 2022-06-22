bd6318b85b3ab01573f3d07be07fe7e06e86ec09
A.L.F. SANTOS <80923022+andrvet1996@users.noreply.github.com>
2022-06-22 12:57:22 -0300

    Update Curso Guanabara-módulo 03.py

--- Curso Guanabara-módulo 03.py
+++ Curso Guanabara-módulo 03.py
@@ -1018,3 +1018,20 @@ def somaPar(lista):
 números = list()
 sorteia(números)
 somaPar(números)
+          
+          
+#Desafio 101-funções para votação
+def voto (ano):
+    from datetime import date #economiza memoria e só usa dentro
+    atual = date.today().year
+    idade = atual - ano
+    if idade < 16:
+        return f'Com {idade} anos: NÃO VOTA.'
+    elif 16 <= idade < 18 or idade >= 65:
+        return f'Com {idade} anos: VOTO OPCIONAL.'
+    else:
+        return f'Com {idade} anos: VOTO OBRIGATÓRIO.'
+
+#Programa principal
+nasc = int(input('Em que ano você nasceu? '))
+print(voto(1976))
