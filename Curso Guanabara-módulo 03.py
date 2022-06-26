50f1e7355f62bd97a904358d597e633fe1871373
A.L.F. SANTOS <80923022+andrvet1996@users.noreply.github.com>
2022-06-26 10:27:25 -0300

    Update Curso Guanabara-módulo 03.py

--- Curso Guanabara-módulo 03.py
+++ Curso Guanabara-módulo 03.py
@@ -1084,4 +1084,23 @@ if n.strip() == '':
 else:
     ficha(n, g)
 
+          
+          
+# Desafio 104-validando entrada de dados
+def leiaInt(msg):
+    ok = False
+    valor = 0
+    while True:
+        n = str(input(msg))
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else:
+            print('\033[0;31mERRO: Digite um número válido.\033[m')#fonte vermelha
+        if ok:
+            break
+    return valor
 
+# programa principal
+n = leiaInt('Digite um número: ')
+print(f'Você acabou de digitar o número {n}.')
