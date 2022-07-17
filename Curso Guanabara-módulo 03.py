6378a024cfacb693e20ad2b97307b3209661aa12
A.L.F. SANTOS <80923022+andrvet1996@users.noreply.github.com>
2022-07-17 13:42:53 -0300

    Update Curso Guanabara-módulo 03.py

--- Curso Guanabara-módulo 03.py
+++ Curso Guanabara-módulo 03.py
@@ -1104,3 +1104,31 @@ def leiaInt(msg):
 # programa principal
 n = leiaInt('Digite um número: ')
 print(f'Você acabou de digitar o número {n}.')
+          
+          
+# Desafio 105-Analisando e gerando dicionários
+def notas (*n, sit=False):
+    '''
+    ->função para analisar notas e faltas de vários alunos
+    :param n: uma ou mais notas dos alunos (aceita várias)
+    :param sit: opcional, caso queira mostrar situaçao coloque site=True
+    :return: retorna dicionário com as informações sobre cada aluno
+    '''
+    r = dict()
+    r['total'] = len(n)
+    r['maior'] = max(n)
+    r['menor'] = min(n)
+    r['média'] = sum(n)/len(n)
+    if sit:
+        if r['média'] >= 7:
+            r['situação'] = 'BOA'
+        elif r['média'] >= 5:
+            r['situação'] = 'RAZOÁVEL'
+        else:
+            r['situação'] = 'RUIM'
+    return r
+
+
+# programa principal
+resp = notas (5.5, 2.5, 9.0, 8.5, sit=True)#site=True aparece situação
+print (resp)
