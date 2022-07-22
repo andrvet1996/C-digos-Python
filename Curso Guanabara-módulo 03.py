9b8bbc7df9660063f184385d6207b0293145094d
A.L.F. SANTOS <80923022+andrvet1996@users.noreply.github.com>
2022-07-22 17:47:04 -0300

    Update Curso Guanabara-módulo 03.py

--- Curso Guanabara-módulo 03.py
+++ Curso Guanabara-módulo 03.py
@@ -1132,3 +1132,46 @@ def notas (*n, sit=False):
 # programa principal
 resp = notas (5.5, 2.5, 9.0, 8.5, sit=True)#site=True aparece situação
 print (resp)
+
+       
+          
+ # Desafio 106-sistema de ajuda interativo em Python.
+from time import sleep
+c = ('\033[m',            #0-sem cor
+     '\033[0; 30; 41m',   #1-vermelho
+     '\033[0; 30; 42m',  #2-verde
+     '\033[0; 30; 43m',  #3-amarelo
+     '\033[0; 30; 44m',  #4-azul
+     '\033[0; 30; 45m',  #5-roxo
+     '\033[7; 30m'        #6-branco
+     );
+
+
+def ajuda (com):
+    título(f'Acessando o manual do comando \'{com}\'', 4)
+    print(c[6], end='')
+    help (com)
+    print(c[0], end='')
+    sleep(2)
+
+
+def título (msg, cor=0):
+    tam = len(msg) + 4
+    print(c[cor], end='')
+    print('~' * tam)
+    print(f' {msg}')
+    print('~' * tam)
+    print(c[0], end='')
+    sleep(1)
+
+#programa principal
+comando = ''
+while True:
+    título('SISTEMA DE AJUDA PyHELP', 2)
+    comando = str(input('Função ou Biblioteca > '))
+    if comando.upper() == 'FIM':
+        break
+    else:
+        ajuda(comando) # ajuda é um comando
+título('ATÉ LOGO!', 1)
+
